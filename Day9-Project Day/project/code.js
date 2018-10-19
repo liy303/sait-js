4d9ed954db9b27c3c59edfa411a77ea4bbbb1538
Lin Yuan <lin@Lins-iMac.hitronhub.home>
2018-10-19 15:10:37 -0600

    day9project in process

--- Day9-Project Day/project/code.js
+++ Day9-Project Day/project/code.js
@@ -1,1 +1,43 @@
 console.log('work!')
+
+// clock
+function updateTime(){
+    document.getElementById('output')
+    .innerHTML=new Date();
+}
+setInterval(updateTime,1000);
+
+// index page navigation (tried to use getELementsByTagname but didn't work, will try later.)
+var link = document.getElementById('li1'); 
+    li1.style.color='white';
+    link.addEventListener("mouseover",function(event){
+        event.preventDefault();
+        link=event.currentTarget;
+        link.style.color='green';
+    })
+    link.addEventListener("mouseout",function(event){
+        link.style.color="white";
+    })
+var link = document.getElementById('li2'); 
+    li2.style.color='white';
+    link.addEventListener("mouseover",function(event){
+        event.preventDefault();
+        link=event.currentTarget;
+        link.style.color='green';
+    })
+    link.addEventListener("mouseout",function(event){
+        link.style.color="white";
+    })
+var link = document.getElementById('li3'); 
+    li3.style.color='white';
+    link.addEventListener("mouseover",function(event){
+        event.preventDefault();
+        link=event.currentTarget;
+        link.style.color='green';
+    })
+    link.addEventListener("mouseout",function(event){
+        link.style.color="white";
+    })
+
+
+// Style
