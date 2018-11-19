974b7c31da9ca6618a85fc009afdf167fc1b07ca
Lin Yuan <lyuan8733@gmail.com>
2018-11-19 00:24:53 -0700

    Udemy-self-learning

---
+++ Udemy-JS/complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -0,0 +1,19 @@
+var firstName = 'Lin';
+var age = 28;
+
+//type coercion
+console.log(firstName+' '+age);
+
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+alert(firstName+' is a '+age+' year old '+job +'.  Is he married?' + isMarried);
+
+//virable mutation
+
+age = 'twenty eight';
+
+var lastName = prompt('what is his last name?');
+console.log(firstName+' '+lastName); 
